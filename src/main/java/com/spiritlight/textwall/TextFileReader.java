39245c2ac91e560389277bffd2fbda8c155c5634
Ori <38721999+OriSpirit@users.noreply.github.com>
2022-05-14 23:11:35 +0800

    +1 Added QoL, config for storing prefix (ModernSoldier req)

--- src/main/java/com/spiritlight/textwall/TextFileReader.java
+++ src/main/java/com/spiritlight/textwall/TextFileReader.java
@@ -1,16 +1,11 @@
 package com.spiritlight.textwall;
 
-import org.jetbrains.annotations.Nullable;
-
-import javax.annotation.Nonnull;
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TextFileReader {
@@ -23,7 +18,8 @@ public class TextFileReader {
             BufferedReader br = new BufferedReader(fr);
             String line;
             while ((line = br.readLine()) != null) {
-                text.add(line);
+                List<String> l = safeChatSize(line);
+                MainMod.messages.addAll(l);
             }
             fr.close();
         } catch (IOException ex) {
@@ -31,5 +27,38 @@ public class TextFileReader {
             ex.printStackTrace();
         }
         return text;
+    }
+
+    // 11pm coding be like
+    private static List<String> safeChatSize(String message) {
+        List<String> l = new ArrayList<>();
+        if(message.length() < 255)
+            return Collections.singletonList(message);
+        int len = message.length();
+        int ml = (256 - MainMod.prefix.length());
+        // int mi = len / ml - (len % ml == 0 ? 0 : 1);
+        int sc = 255;
+        char[] c = {'.', ',', ' '}; // by priority
+        while(len > (ml - MainMod.prefix.length())) {
+            int m;
+            int n = 256;
+            int[] in = {-1, -1, -1};
+            for(int x=0; x<c.length; x++) {
+                m = message.indexOf(c[x]); // acq int ind
+                if(ml - m < n && m < ml) { // we want close to 0 as possible
+                    in[x] = m;
+                }
+                n = m;
+            }
+            for (int i : in) {
+                if (i != -1) {
+                    sc = i;
+                    break;
+                }
+            }
+            l.add(message.substring(0, sc));
+            message = message.substring(sc);
+        }
+        return l;
     }
 }
